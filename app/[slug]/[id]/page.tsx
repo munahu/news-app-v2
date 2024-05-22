3ae7cff87acd6a8821eb9ee12fdfbc3404a016e3
Muna Hussen <muna.hussen@hotmail.ca>
2024-05-22 10:51:20 -0400

    fix: blurred image background not extending past layout component

--- app/[slug]/[id]/page.tsx
+++ app/[slug]/[id]/page.tsx
@@ -22,61 +22,63 @@ export default function Page({
 
   if (article.id === params.id) {
     return (
-      <Layout heading={params.slug}>
-        <div className="relative h-screen">
-          <MobileTextLayout
-            title={article.title}
-            description={article.description}
-            urlToImage={article.urlToImage}
-            url={article.url}
-          />
-          <div className="hidden md:flex mt-16 absolute z-10">
-            <div className="w-2/3 mr-12 lg:w-1/2">
-              <h2 className="text-4xl mb-6 font-semibold -tracking-[1.5px]">
-                {article.title}
-              </h2>
-              <p className="text-xl font-light -tracking-[0.3px] mb-12">
-                {article.description}
-              </p>
-              <a
-                href={article.url}
-                target="_blank"
-                className="flex items-center justify-center w-fit px-5 py-3 rounded-3xl text-sm font-light -tracking-[0.3px] bg-neutral-200 text-black"
-              >
-                <span className="mr-1">Read full article</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                  />
-                </svg>
-              </a>
-            </div>
-            <Image
-              alt={article.title}
-              src={article.urlToImage}
-              width={1200}
-              height={550}
-              className="object-cover w-1/3 lg:w-1/2"
+      <>
+        <Image
+          alt={article.title}
+          src={article.urlToImage}
+          width={1200}
+          height={550}
+          className="h-full object-cover blur-[100px] brightness-50 absolute"
+        />
+        <Layout heading={params.slug}>
+          <div className="relative h-screen">
+            <MobileTextLayout
+              title={article.title}
+              description={article.description}
+              urlToImage={article.urlToImage}
+              url={article.url}
             />
+            <div className="hidden md:flex mt-16 absolute z-10">
+              <div className="w-2/3 mr-12 lg:w-1/2">
+                <h2 className="text-4xl mb-6 font-semibold -tracking-[1.5px]">
+                  {article.title}
+                </h2>
+                <p className="text-xl font-light -tracking-[0.3px] mb-12">
+                  {article.description}
+                </p>
+                <a
+                  href={article.url}
+                  target="_blank"
+                  className="flex items-center justify-center w-fit px-5 py-3 rounded-3xl text-sm font-light -tracking-[0.3px] bg-neutral-200 text-black"
+                >
+                  <span className="mr-1">Read full article</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                    />
+                  </svg>
+                </a>
+              </div>
+              <Image
+                alt={article.title}
+                src={article.urlToImage}
+                width={1200}
+                height={550}
+                className="object-cover w-1/3 lg:w-1/2"
+              />
+            </div>
           </div>
-          <Image
-            alt={article.title}
-            src={article.urlToImage}
-            width={1200}
-            height={550}
-            className="h-2/3 object-cover blur-[100px] brightness-50 border border-white"
-          />
-        </div>
-      </Layout>
+        </Layout>
+      </>
     );
   } else {
     notFound();
