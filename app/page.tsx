960e7703a7ed44610a2d3c523d58224a0fa5671a
Muna Hussen <muna.hussen@hotmail.ca>
2024-05-22 12:01:01 -0400

    fix: category name not on correct category item

--- app/page.tsx
+++ app/page.tsx
@@ -14,20 +14,18 @@ async function PreviewCategory({ category }: PreviewCategoryProps) {
     <li className="relative cursor-pointer group">
       {articles &&
         articles.map((article, index) => (
-          <Link key={index} href={`/${category}`} className="relative">
+          <Link key={index} href={`/${category}`}>
             {index === 0 && (
               <>
-                <span className="absolute left-2 bottom-6 text-white text-5xl md:text-6xl lg:text-7xl capitalize font-semibold -tracking-[0.04em] font-sans z-40 opacity-65 group-hover:opacity-100">
+                <span className="absolute left-4 lg:left-7 bottom-3 text-white text-5xl md:text-6xl lg:text-7xl capitalize font-semibold -tracking-[0.04em] font-sans z-40 opacity-65 group-hover:opacity-100">
                   {category}
                 </span>
-
                 <Image
                   alt={article.title}
                   src={article.urlToImage}
                   width={1200}
-                  height={8250}
-                  className="h-full object-cover brightness-50 tracking-wider"
-                  priority
+                  height={550}
+                  className="h-full w-full object-cover brightness-50 group-hover:brightness-75 tracking-wider"
                 />
               </>
             )}
@@ -39,7 +37,7 @@ async function PreviewCategory({ category }: PreviewCategoryProps) {
 
 export default async function Home() {
   return (
-    <ul className="h-screen relative grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+    <ul className="h-screen grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
       {categories.map((category, index) => (
         <PreviewCategory key={index} category={category} />
       ))}
